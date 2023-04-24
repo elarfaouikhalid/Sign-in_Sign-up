9e49fce1143073f42d63aa061caf9b5b3accd2d2
ELARFAOUI KHALID <75584334+elarfaouikhalid@users.noreply.github.com>
2023-04-24 17:30:34 +0100

    Update server.php

--- server.php
+++ server.php
@@ -1,59 +1,52 @@
 <?php
 session_start();
-$errors=array();
- $db=mysqli_connect('127.0.0.1','root','','registre')or die("cold not connect to database");
-// $dsn='mysql:host=localhost;dbname=registre';
-// $option=array(PDO::MYSQL_ATTR_INIT_COMMAND =>  "SET username 'UTF8'");
-// $db=new DPO($dsn,'root','',$option);
+$errors = [];
+
+try {
+    $pdo = new PDO('mysql:host=localhost;dbname=registre', 'root', '');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Could not connect to the database: " . $e->getMessage());
+}
+
 if(isset($_POST['register'])){
-$username=$_POST['username'];
-$email=$_POST['email'];
-$password1=$_POST['password'];
-$password2=$_POST['password2'];
-if(empty($username) || empty($email) || empty($password1) || $password1!=$password2)
-{
-array_push($errors,'information required');
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password1 = $_POST['password'];
+    $password2 = $_POST['password2'];
+
+    if(empty($username) || empty($email) || empty($password1) || $password1 != $password2) {
+        array_push($errors, 'Information is required');
+    } else {
+        $stmt = $pdo->prepare("INSERT INTO user(username, email, password) values(?, ?, ?)");
+        $password_hash = password_hash($password1, PASSWORD_DEFAULT);
+        $stmt->execute([$username, $email, $password_hash]);
+        $_SESSION['username'] = $username;
+        header('location: index1.php');
+        exit();
+    }
 }
-else{
-$sql=" INSERT INTO user(username,email,password) values('$username','$email','$password1') ";
-$res=mysqli_query($db,$sql);
-// $db->exec($sql);
-    $_SESSION['username']=$username;
-    // $_SESSION['success']="you are now logged ";
-    header('location:index1.php');
-    mysqli_close($db);
-}
-}
-if(isset($_GET['logout']))
-{
+
+if(isset($_GET['logout'])) {
     session_destroy();
     unset($_SESSION['username']);
     header("location: login.php");
+    exit();
 }
-
 
 if(isset($_POST['login'])){
-    $username=mysqli_real_escape_string($db,$_POST['username']);
-    $password=mysqli_real_escape_string($db,$_POST['password']);
-    // if(empty($username))
-    // {
-    // array_push($errors,'username required');
-    // }
-    // if(empty($password))
-    // {
-    // array_push($errors,'password required');
-    // }
-    $qeury="select * from user where username='$username' and password='$password' ";
-    $result=mysqli_query($db,$qeury);
-    if(mysqli_num_rows($result))
-    {
-        $_SESSION['username']=$username;
-        // $_SESSION['success']="login succesfuly ";
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $stmt = $pdo->prepare("SELECT * FROM user WHERE username = ?");
+    $stmt->execute([$username]);
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if($user && password_verify($password, $user['password'])) {
+        $_SESSION['username'] = $username;
         header('location: index1.php');
-    }
-    else
-    {
-        array_push($errors,'invalid password or name');
+        exit();
+    } else {
+        array_push($errors, 'Invalid username or password');
     }
 }
-?>
